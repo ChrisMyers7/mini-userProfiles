10176e553e4289d9b8d1a78ed06eb23db3354712
Chris Myers <7chrismmyers7@gmail.com>
2016-05-24 11:54:40 -0600

    Mini project complete

--- controller.js
+++ controller.js
@@ -1,5 +1,13 @@
-angular.module('userProfiles').controller('MainController', function($scope){
+angular.module('userProfiles').controller('MainController', function($scope, mainService){
     
     $scope.thisAppIsBroken = "This angular app is working";
    
+    $scope.aquireUsers = function() {
+        $scope.users = mainService.getUsers();
+    }
+    
+    $scope.aquireUsers();
+    
+    $scope.toggle = mainService.toggleFavorite;
+    
 })
